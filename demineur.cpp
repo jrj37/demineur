d14c41e807a743d96ad27dba7a8b771243cfca7d
jrj37 <jrjulien@JR>
2024-02-28 16:39:09 +0100

    caché

--- demineur.cpp
+++ demineur.cpp
@@ -9,24 +9,29 @@ Demineur::Demineur(){
     std::uniform_int_distribution<int> distribution(0, 1); 
     for (int i=0;i<10;i++){
         std::vector<int> vector;
+        std::vector<std::pair<std::string,int>> vector2;
         for (int i=0;i<10;i++){
-            vector.push_back(distribution(engine));
+            int rd=distribution(engine);
+            vector.push_back(rd);
+            std::pair<std::string,int> p("#",rd);
+            vector2.push_back(p);
             }
         tab.push_back(vector);
+        tab2.push_back(vector2);
     }
     }
 void Demineur::affiche(){
-    for (auto const& row : tab) {
+    for (auto const& row : tab2) {
         for (auto const& elem : row) {
-            std::cout << elem << " ";
+            std::cout << elem.first << " ";
         }
         std::cout << std::endl;
     }
 }
 int Demineur::parcours(){
-    for (auto const& row: tab){
+    for (auto const& row: tab2){
         for (auto const&elem :row){
-            if (elem==1){
+            if (elem.second==1){
                 std::cout<< "FIN DU JEU"<<std::endl;
                 return 1;
             }
--- demineur.h
+++ demineur.h
@@ -5,8 +5,10 @@ class Demineur{
     Demineur();
     void affiche();
     int parcours();
+    void hide();
     private:
     int bombe;
     int drapeau;
     std::vector<std::vector<int>> tab;
+    std::vector<std::vector<std::pair<std::string,int>>> tab2;
 };
